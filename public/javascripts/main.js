f8c3efd2e3b0293221a0c54fe1243d0af2b114a8
FUSSE FAUSTO <43719026@ort>
2018-10-16 09:03:40 -0300

    dfsf

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,7 +14,7 @@ var videoUser = document.querySelector('#videoUser');
 	videoUser.srcObject = stream;
 });*/
 console.log(location.hostname);
-var peer = new Peer(userId, {host: location.hostname, port: 3000, path: '/peerjs'});
+var peer = new Peer(userId, {host: location.hostname, port: 3000, path: '/peerjs', secure:true});
 
 function pingHeroku() {
     peer.socket.send({type: 'ping'});
