c383f15230aee3c26e5a0b06bcf9e0e7472915b7
Fausto Fusse <faustofusse@gmail.com>
2018-10-16 18:01:54 -0300

    ok

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,7 +15,12 @@ var videoUser = document.querySelector('#videoUser');
 });*/
 
 console.log(location.hostname);
-var peer = new Peer(userId, {host: location.hostname, port: location.port || (location.protocol === 'https:' ? 443 : 80), secure:true});
+var peer = new Peer(userId, { 
+	host: location.hostname, 
+	port: location.port || (location.protocol === 'https:' ? 443 : 80), 
+	secure: true,
+	path:'/peerjs'
+});
 
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
