150332796f37929dee78b59fc3d44bd6e812c211
Fausto Fusse <faustofusse@gmail.com>
2018-10-22 20:11:55 -0300

    stun

--- public/javascripts/videocalls.js
+++ public/javascripts/videocalls.js
@@ -14,7 +14,7 @@ var peer = new Peer(userId, {
 	port: location.port || (location.protocol === 'https:' ? 443 : 80), 
 	secure: (location.protocol === 'https:'),
 	config: {'iceServers': [
-		{ url: 'stun1.l.google.com:19302' },
+		{url:'stun:stun.l.google.com:19302'},
 		{
 			url: 'turn:numb.viagenie.ca',
 			credential: 'muazkh',
