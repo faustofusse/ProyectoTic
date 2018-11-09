05c815356f12dcbfda6081f354f4da280a0657b0
FUSSE FAUSTO <43719026@ort>
2018-11-09 15:28:12 -0300

    ice server list

--- public/javascripts/videocalls.js
+++ public/javascripts/videocalls.js
@@ -16,23 +16,40 @@ var peer = new Peer(userId, {
 	port: location.protocol === 'https:' ? 443 : 80, 
 	secure: (location.protocol === 'https:'),
 	config: {'iceServers': [
-		//{url:'stun:sp-turn1.xirsys.com'},
+		{url:'stun:stun01.sipphone.com'},
+		{url:'stun:stun.ekiga.net'},
+		{url:'stun:stun.fwdnet.net'},
+		{url:'stun:stun.ideasip.com'},
+		{url:'stun:stun.iptel.org'},
+		{url:'stun:stun.rixtelecom.se'},
+		{url:'stun:stun.schlund.de'},
 		{url:'stun:stun.l.google.com:19302'},
+		{url:'stun:stun1.l.google.com:19302'},
+		{url:'stun:stun2.l.google.com:19302'},
+		{url:'stun:stun3.l.google.com:19302'},
+		{url:'stun:stun4.l.google.com:19302'},
+		{url:'stun:stunserver.org'},
+		{url:'stun:stun.softjoys.com'},
+		{url:'stun:stun.voiparound.com'},
+		{url:'stun:stun.voipbuster.com'},
+		{url:'stun:stun.voipstunt.com'},
+		{url:'stun:stun.voxgratia.org'},
+		{url:'stun:stun.xten.com'},
 		{
-			url:'turn:sp-turn1.xirsys.com:80?transport=udp',
-			credential:'eebe899c-e0fc-11e8-8276-c35ebd4a49e7',
-			username:'eebe88fc-e0fc-11e8-b9c1-4d6b2edf0bba'
-		}
-		/*{
-			url: 'turn:numb.viagenie.ca',
-			credential: 'F@usto123',
-			username: 'faustofusse@gmail.com'
-		}*/	
-		/*{
 			url: 'turn:numb.viagenie.ca',
 			credential: 'muazkh',
 			username: 'webrtc@live.com'
-		}*/
+		},
+		{
+			url: 'turn:192.158.29.39:3478?transport=udp',
+			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+			username: '28224511:1379330808'
+		},
+		{
+			url: 'turn:192.158.29.39:3478?transport=tcp',
+			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+			username: '28224511:1379330808'
+		}
 	]}
 });
 
