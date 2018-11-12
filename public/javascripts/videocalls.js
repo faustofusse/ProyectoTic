8708a6449f736d858e56d8f8a33f1445b55b509f
Fausto Fusse <faustofusse@gmail.com>
2018-11-12 00:12:50 -0300

    iceServers

--- public/javascripts/videocalls.js
+++ public/javascripts/videocalls.js
@@ -19,7 +19,7 @@ var peer = new Peer(userId, {
 	config: {
 		// iceTransportPolicy: 'relay',
 		iceServers: [
-			{ url: 'stun.samsungsmartcam.com:3478' },
+			{ url: 'stun:stun.samsungsmartcam.com:3478' },
 			{ url: 'stun:stun.stunprotocol.org' },
 			{ url: 'stun:stun.l.google.com:19302' },
 	    	{
