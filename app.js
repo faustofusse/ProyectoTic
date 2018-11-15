8db9fd59d5bca09ec0b7675313a9de05b0252750
Fausto Fusse <faustofusse@gmail.com>
2018-11-14 22:40:10 -0300

    probando xirsys

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.locals.users = [];
 app.locals.connections = [];
 app.locals.videocalls = [];
 
+// XIRSYS
+var webrtc = require('./routes/webrtc.js');
+var xirsys = require('./config/xirsys');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -68,6 +72,7 @@ app.use(function(req, res, next){
 app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/api', apiRouter);
+app.use('/webrtc', webrtc(xirsys));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/xirsys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "gateway": "global.xirsys.net",
+    "info": {
+        "ident": "faustofusse",
+        "secret": "ad71b392-e0fc-11e8-8374-0242ac110003",
+        "channel": "tars",
+        "secure":true
+    },
+    "allowedServices": ["/_token", "/_host", "/_data", "/_ns", "/_turn"],
+    "overrideAllowedChannel": false
+}
